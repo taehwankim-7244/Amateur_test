ec2191ca9a747669140d2fda41c76b628bd7dc9c
narnhada <kbh91122@naver.com>
2020-07-05 15:57:03 +0900

    first

---
+++ src/test/java/com/amateur/amateur/AmateurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amateur.amateur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmateurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
